affd3dc1fe051a0b138922a4c965a592f4a1328a
Alfie Gunn <alfie.gunn@students.mq.edu.au>
2024-12-02 22:34:57 +1100

    Day 2, part 1

---
+++ day2/part1/src/level.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+enum Slope {down, up};
+
+Slope getSlope(int16_t data);
+
+class Level
+{
+private:
+    std::vector<int16_t> data;
+    std::vector<int16_t> diff;
+
+    void create_diff();
+    bool validate_direction();
+    bool validate_slope();
+
+public:
+    void add(int16_t data);
+    void from_string(std::string input);
+    bool validate();
+};
